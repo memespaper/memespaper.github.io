99cb7d6f0ca1d997bfff3ca783ebcfd7cf9086ee
memespaper <39280726+memespaper@users.noreply.github.com>
2018-05-14 23:54:41 +0200

    graph file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "memes.gexf",
+    graphFile : "memes_all.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
